ec8b06bc7c0a1b01e293dcb11bd67e55c22a7531
Matej Janežič <janezic.mj@gmail.com>
2024-10-01 18:16:54 +0200

    fix: edgecase behaviour for entrypoint.sh

--- docker/local/entrypoint.sh
+++ docker/local/entrypoint.sh
@@ -2,18 +2,30 @@
 
 set -eu
 
-dirs="/appdata/media"
+dirs=(
+    "/appdata/media"
+)
 
-if [[ -z ${USER_UID} ]] | [[ -z ${USER_GID} ]]; then
+if [[ ${USER_UID:+x} != "x" ]] || [[ ${USER_GID:+x} != "x" ]]; then
     # using docker desktop
+    echo "Assuming Docker Desktop. Not changing permissions"
+    echo "Running 'exec $@'"
     exec "$@"
 else
+    echo "Assuming not Docker Desktop. Changing permissions for specified folders"
     # not using docker desktop
-    for d in $dirs; do
-        if ! [[ "$(stat -c '%u:%g' $d)" == "$USER_UID:$USER_GID" ]]; then
+    for d in ${dirs[@]}; do
+        if [[ -d "$d" ]] && [[ "$(stat -c '%u:%g' $d)" != "$USER_UID:$USER_GID" ]]; then
+            # TODO:(matej) what if directory is owned but content is not?
+            echo "Found not owned directory '$d'. Running `chown -R $USER_UID:$USER_GID $d`"
             chown -R "$USER_UID:$USER_GID" "$d"
+        else
+            echo "Specified directory '$d' not found"
+
         fi
     done
 
+    echo "Running 'exec gosu $USER_UID:$USER_GID $@'"
     exec gosu "$USER_UID:$USER_GID" "$@"
 fi
+
--- docker/remote/entrypoint.sh
+++ docker/remote/entrypoint.sh
@@ -2,12 +2,21 @@
 
 set -eu
 
-dirs="/appdata/static /appdata/media"
+dirs=(
+    "/appdata/static"
+    "/appdata/media"
+)
 
-for d in $dirs; do
-    if ! [[ "$(stat -c '%u:%g' $d)" == "$USER_UID:$USER_GID" ]]; then
+for d in ${dirs[@]}; do
+    if [[ -d "$d" ]] && [[ "$(stat -c '%u:%g' $d)" != "$USER_UID:$USER_GID" ]]; then
+        # TODO:(matej) what if directory is owned but content is not?
+        echo "Found not owned directory '$d'. Running `chown -R $USER_UID:$USER_GID $d`"
         chown -R "$USER_UID:$USER_GID" "$d"
+    else
+        echo "Specified directory '$d' not found"
+
     fi
 done
 
+echo "Running 'exec gosu $USER_UID:$USER_GID $@'"
 exec gosu "$USER_UID:$USER_GID" "$@"
