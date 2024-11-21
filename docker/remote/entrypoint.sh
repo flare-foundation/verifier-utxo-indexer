63f70234c55db0e132a97820f5a70efd99af5a8d
filipdf <filip@kravapolde.com>
2024-11-21 13:44:47 +0100

    fix entrypoint

--- docker/remote/entrypoint.sh
+++ docker/remote/entrypoint.sh
@@ -7,16 +7,22 @@ dirs=(
     "/appdata/media"
 )
 
-for d in ${dirs[@]}; do
-    if [[ -d "$d" ]] && [[ "$(stat -c '%u:%g' $d)" != "$USER_UID:$USER_GID" ]]; then
-        # TODO:(matej) what if directory is owned but content is not?
-        echo "Found not owned directory '$d'. Running `chown -R $USER_UID:$USER_GID $d`"
-        chown -R "$USER_UID:$USER_GID" "$d"
-    else
-        echo "Specified directory '$d' not found"
+if [[ ${USER_UID:+x} != "x" ]] || [[ ${USER_GID:+x} != "x" ]]; then
+    echo "Running with current user, USER_UID or USER_GID is unset"
+    echo "Running 'exec $@'"
+    exec "$@"
+else
+    echo "Running with user and group ${USER_UID}:${USER_GID}"
+    for d in ${dirs[@]}; do
+        if [[ -d "$d" ]] && [[ "$(stat -c '%u:%g' $d)" != "$USER_UID:$USER_GID" ]]; then
+            echo "Found not owned directory '$d'. Running 'chown $USER_UID:$USER_GID $d'"
+            chown "$USER_UID:$USER_GID" "$d"
+        elif ! [[ -d "$d" ]]; then
+            echo "Specified directory '$d' not found"
+        fi
+    done
 
-    fi
-done
+    echo "Running 'exec gosu $USER_UID:$USER_GID $@'"
+    exec gosu "$USER_UID:$USER_GID" "$@"
+fi
 
-echo "Running 'exec gosu $USER_UID:$USER_GID $@'"
-exec gosu "$USER_UID:$USER_GID" "$@"
